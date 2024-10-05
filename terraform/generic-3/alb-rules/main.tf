9a5c350d77bf7c3e5cdaf6f01af321f385f23cfd
shawnly <shawnly@hotmail.com>
2024-10-04 22:12:25 -0700

    update

--- terraform/generic-3/alb-rules/main.tf
+++ terraform/generic-3/alb-rules/main.tf
@@ -1,25 +1,25 @@
-resource "aws_lb_listener_rule" "microservice_forwarding_rule" {
-  count        = length(var.forwarding_rules)
-  listener_arn = var.listener_arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.microservice_target_group[count.index].arn
-  }
-
-  condition {
-    path_pattern {
-      values = [var.forwarding_rules[count.index].forward_path]
-    }
-  }
-
-  priority = var.forwarding_rules[count.index].priority
+# Parse DomainEC2Map into domain, environment, and ec2
+locals {
+  domain      = split("-", var.DomainEC2Map)[0]
+  environment = split("-", var.DomainEC2Map)[1]
+  ec2         = split("-", var.DomainEC2Map)[2]
 }
 
+# Use the parsed domain and environment to find the EC2 instance by tag
+data "aws_instance" "selected_ec2" {
+  filter {
+    name   = "tag:DomainEC2Map"
+    values = [var.DomainEC2Map]
+  }
+
+  count = 1
+}
+
+# Reference the selected EC2 instance ID
 resource "aws_lb_target_group" "microservice_target_group" {
   count = length(var.forwarding_rules)
 
-  name = lower(replace("${var.project}-${var.environment}-${var.forwarding_rules[count.index].name}", "_", "-"))
+  name = lower(replace("${local.domain}-${local.environment}-${var.forwarding_rules[count.index].name}", "_", "-"))
 
   port        = var.forwarding_rules[count.index].port
   protocol    = "HTTP"
@@ -32,19 +32,11 @@ resource "aws_lb_target_group" "microservice_target_group" {
     timeout             = 5
     healthy_threshold   = 3
     unhealthy_threshold = 3
-    matcher {
-      http_code = var.forwarding_rules[count.index].response_code  # Use the new variable here
-    }
+    matcher             = var.forwarding_rules[count.index].response_code
   }
-}
 
-
-
-
-resource "aws_lb_target_group_attachment" "ec2_attachment" {
-  count = length(var.forwarding_rules)
-
-  target_group_arn = aws_lb_target_group.microservice_target_group[count.index].arn
-  target_id        = var.forwarding_rules[count.index].ec2_instance_id
-  port             = var.forwarding_rules[count.index].port
+  target {
+    id = data.aws_instance.selected_ec2[0].id
+    port = var.forwarding_rules[count.index].port
+  }
 }
